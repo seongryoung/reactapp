e7fb5099ba0764ba3214489feb4bcfd35f7833e4
최성령 <seong@choeseonglyeong-ui-MacBook-Pro.local>
2019-12-03 17:11:17 +0900

    change function Component By Class Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import prop from 'prop-types'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    count : 0
+  }
+ 
+  add = () => {
+    this.setState(current =>({count : current.count + 1}))
+  }
+  minus = () => {
+    this.setState(current => ({count : current.count - 1}))
+  }
+
+  componentDidUpdate = () => {
+    console.log("Compnent did updated")
+  }
+
+  componentDidMount = () => {
+    console.log("component did Mounted")
+  }
+
+  componentWillUnmount = () => {
+    console.log("Component did Unmounted")
+  }
+
+  render(){
+    console.log("i am renderling");
+    return(
+      <div>
+        <h1>the number is : {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    )
+  }
 }
-
+ 
 export default App;
