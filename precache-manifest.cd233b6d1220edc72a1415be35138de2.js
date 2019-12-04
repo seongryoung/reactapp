58dc3514f7e39b78acbdd78b30e5d222e466bf1b
최성령 <seong@choeseonglyeong-ui-MacBook-Pro.local>
2019-12-04 17:50:28 +0900

    Updates

---
+++ precache-manifest.cd233b6d1220edc72a1415be35138de2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de4c33034d12e7736bbb7d9a078cfa6",
+    "url": "/movieApp2019/index.html"
+  },
+  {
+    "revision": "facdee0d1dd8c3724450",
+    "url": "/movieApp2019/static/css/main.3151a084.chunk.css"
+  },
+  {
+    "revision": "9e0b343a940520eb6b6e",
+    "url": "/movieApp2019/static/js/2.c17f3bf8.chunk.js"
+  },
+  {
+    "revision": "facdee0d1dd8c3724450",
+    "url": "/movieApp2019/static/js/main.ac4a9361.chunk.js"
+  },
+  {
+    "revision": "942455589c1f08ac13dd",
+    "url": "/movieApp2019/static/js/runtime-main.999dfe18.js"
+  }
+]);
